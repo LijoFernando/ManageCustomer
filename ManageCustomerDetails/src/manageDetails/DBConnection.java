bedbc966eae14721d2d788d6fcc8ec2a05fc0a38
LijoFernando <rotricklijo@gmail.com>
2021-08-06 08:04:47 +0530

    new loadmethod

--- ManageCustomerDetails/src/manageDetails/DBConnection.java
+++ ManageCustomerDetails/src/manageDetails/DBConnection.java
@@ -5,15 +5,20 @@ public class DBConnection {
     //Class.forName("com.mysql.jdbc.Driver");
     void insertDetailToDB(String name , Date date) {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/customer", "root", "Q@W#E$R%");
             PreparedStatement preparedStatement = con.prepareStatement("insert into CustomerInfo (Name,Birth) values (?,?)");
             preparedStatement.setString(1,name);
             preparedStatement.setDate(2,date);
             con.close();
             System.out.println("Record inserted");
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+    void loadToHash(){
+        try {
+
+
         }
     }
 
---
+++ ManageCustomerDetails/src/manageDetails/LoadData.java
@@ -0,0 +1,11 @@
+package manageDetails;
+
+import com.sun.org.apache.xml.internal.serializer.SerializerTrace;
+
+import java.util.HashSet;
+
+public class LoadData {
+    void loadHasMap() {
+
+    }
+}
